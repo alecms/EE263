55b03f2482aec2268a9989057bc3d21527426b33
alecms <alec@velocitek.com>
2015-12-26 06:45:41 +0000

    Starts trying to plot z vs. t

--- 2_20/plot_demo.py
+++ 2_20/plot_demo.py
@@ -1,9 +1,25 @@
 import matplotlib
+import math
+
+def zed(t):
+    result = 5 * math.sin(t / 10 + 2) + 0.1 * math.sin(t) + 0.1 * math.sin(2 * t - 5)
+    return result
+
+
+z = []
+t = range(0, 100)
+
+for i in t:
+    z.append(zed(i))
+
+
+print z
+
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(range(100))
+plt.scatter(z, t)
+plt.show()
 
 fig.savefig("graph.png")
